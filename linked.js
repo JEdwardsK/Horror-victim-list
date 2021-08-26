b5a319d7d71aeb67636662ae21d8c21ed40cb5ae
J Edwards <jesse.edwards2@protonmail.com>
2021-08-26 18:52:14 -0400

    started linked list creation, tdd

--- linked.js
+++ linked.js
@@ -1,4 +1,26 @@
 
-export class CharacterNode {
 
+export class CharacterNode {
+  constructor (name, score) {
+    // if (typeof score !== 'number') throw new Error('invalid type, score must be a number')
+    this.name = name
+    this.score = score
+  }
+}
+
+export class CharacterLinkedList {
+  constructor () {
+    this.head = null
+    this.next = null
+    this.size = 0
+  }
+
+  addCharacter(character) {
+    if (!this.head) {
+      this.head = character
+      this.size++
+      return
+    }
+    if(this.head)
+  }
 }
--- linked.test.js
+++ linked.test.js
@@ -1,7 +1,65 @@
-import { CharacterNode } from './linked'
+import { CharacterLinkedList, CharacterNode } from './linked'
 
+const dummyCharacter = new CharacterNode('amy', 10)
 
-test('should be able to create a character', () => {
-  const testChar = new CharacterNode('testChar')
-  expect(testChar.name).toBe('testChar')
+test('character has name', () => {
+  expect(dummyCharacter.name).toEqual('amy')
 })
+
+test('character has score', () => {
+  expect(dummyCharacter.score).toEqual(10)
+})
+
+test('character score should be number type', () => {
+
+  expect(typeof dummyCharacter.score).toEqual('number')
+})
+
+
+const dummyList = new CharacterLinkedList()
+
+test('should have head attribute', () => {
+  expect(dummyList.head).toBeDefined()
+})
+
+test('should have next attribute', () => {
+  expect(dummyList.next).toBeDefined()
+})
+
+test('should have size attribute', () => {
+  expect(typeof dummyList.size).toBe('number')
+})
+
+
+test('should have an add character function', () => {
+  expect(dummyList.addCharacter).toBeDefined()
+})
+
+test('if empty list, input character should become head', () => {
+  const emptyList = new CharacterLinkedList()
+  emptyList.addCharacter(dummyCharacter)
+  expect(emptyList.head).toEqual(dummyCharacter)
+})
+
+test('size attribute should increase when a character is added', () => {
+  console.log(dummyList)
+  const formerSize = dummyList.size
+  console.log(formerSize)
+  dummyList.addCharacter(dummyCharacter)
+  console.log(dummyList)
+  const newSize = dummyList.size
+  console.log(newSize)
+  expect(newSize).toEqual(formerSize + 1)
+})
+
+
+test('if character score is less than head, prepend', () => {
+  const secondCharacter = new CharacterNode('james', 2)
+  dummyList.addCharacter(secondCharacter)
+  expect(dummyList.size).toEqual(2)
+})
+
+
+
+
+
