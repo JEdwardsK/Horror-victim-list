c03a888497dd754b97c1d3657aa0980049c90765
J Edwards <jesse.edwards2@protonmail.com>
2021-08-30 12:21:43 -0400

    added some questions

--- linked.js
+++ linked.js
@@ -91,3 +91,83 @@ export const coinToss = () => {
   // return result === 0 ? -1 : 1
   return -1
 }
+
+export const scores = {
+  child: 1,
+  teenager: 5,
+  adult: 4,
+  OAP: 3,
+  virgin: 1,
+  Slut: 10,
+  Hero: 1,
+  Villain: 9,
+  BlackGuy: 9000,
+  FinalGirl: -9000,
+  EthnicMinority: 20,
+  White: 1,
+  redShirt: -10,
+  male: 5,
+  female: 7,
+  police: 4,
+  mentor: 5,
+  scientists: 5,
+  niceGuy:
+
+}
+
+export const questions = {
+  age: 'how old is the character?',
+  ethnicity: 'what ethnicity is the character?',
+  isProtagonist: 'is the character the protagonist?',
+  isAntagonist: 'is the character the main villain?',
+  gender: 'is the character male or female?',
+  occupation: 'what does the character do for a living?',
+  isRelated: 'is the character related to the protagonist?',
+  isMentallyDisabled: 'does the character have a mental disability?',
+  isPhysicallyDisabled: 'does the character have a physical disability?',
+  isNiceGuy: 'is the character generally the most average amongst the group'
+}
+
+export const bonus = {
+  woods: 'does the character enjoy exploring poorly lit wooded areas?',
+  hauntedHouse: 'is the character\'s perfect group holiday a visit to an abandoned murder house?',
+  shortcuts: 'does the character prefer to take an unexplored, untested \'shortcut\' to reach a destination, instead of following a planned route, or asking for directions',
+  confirmTheKill: 'if the character was to successfully injure or overpower a villain, would they proceed to kill, maim, or otherwise incapacitate them, or would they run away?',
+  confirmTheKillBonus: 'when running away from the downed baddie, will they drop their weapon next to the baddie?',
+  everythingUnlocked: 'does the character lock doors behind them when it is logical to do so?',
+  scoobyGang: 'Does the character recommend splitting up to search an area?',
+  loneWanderer: 'Would the character drift away from the party without telling anyone where they are going or why?',
+  peerPressure:'if the answer was no to any of the previous questions, would the character cave to the demands of the rest of the group and follow along (they may mumble or whine that it is a bad idea after the fact)?'
+}
+
+
+
+//TODO: write a function that returns a reasonable number if a user requests a random score be generated. This is based on a string identifying how likely the user thinks the character is to die
+const randomScore = modifier => {
+
+  const randomNumber = Math.floor(Math.random() * 90)
+  let score = 0
+  switch (modifier) {
+    case 'immortal':
+      score = 100 // some high score
+      break;
+    case 'very unlikely':
+      score = randomNumber * 20
+      break;
+    case 'unlikely':
+      score = randomNumber * 10
+      break;
+    case 'likely':
+      score = randomNumber * 
+      break;
+    case 'very likely':
+      score = randomNumber * 10
+      break;
+    case 'red shirt':
+      score = randomNumber * -10
+      break;
+
+    default:
+      break;
+  }
+}
