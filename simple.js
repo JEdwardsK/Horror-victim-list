2ba379fe352b99a49d497e09777af97376703ca8
J Edwards <jesse.edwards2@protonmail.com>
2021-09-01 13:57:47 -0400

    refined, made question object into function

--- simple.js
+++ simple.js
@@ -36,88 +36,34 @@ for (let i = 0; i < characterArchetypes.length; i++) {
 
 let characterName = ''
 
+const questionConstructor = question => {
+  return {
+    name: 'response', 
+    pattern: /([yn])+/i,
+    message: 'valid response must be "y" for yes or "n" for no',
+    description: `${question} (Y/N)`,
+    required: true
+  }
+}
+
 const questionName = { 
   description: 'What is the character\'s name?',
   required: true,
   name: 'response'
 }
-const questionBlack = { 
-  name: 'response', 
-  pattern: /([yn])+/i,
-  message: 'valid response must be "y" for yes or "n" for no',
-  description: 'Is the character Black? (Y/N)',
-  required: true
-}
-const questionToken = { 
-  name: 'response', 
-  pattern: /([yn])+/i,
-  message: 'valid response must be "y" for yes or "n" for no',
-  description: 'Is the character from a minority group? (Y/N)',
-  required: true
-}
-const questionFirstChar = { 
-  name: 'response', 
-  pattern: /([yn])+/i,
-  message: 'valid response must be "y" for yes or "n" for no',
-  description: 'Is the character the first character on screen? (Y/N)',
-  required: true
-}
-const questionGender = { 
-  name: 'response', 
-  pattern: /([yn])+/i,
-  message: 'valid response must be "y" for yes or "n" for no',
-  description: 'Is the character the female? (Y/N)',
-  required: true
-}
-const questionVirgin = { 
-  name: 'response', 
-  pattern: /([yn])+/i,
-  message: 'valid response must be "y" for yes or "n" for no',
-  description: 'Is the character a virgin? (Y/N)',
-  required: true
-}
-const questionWhore = { 
-  name: 'response', 
-  pattern: /([yn])+/i,
-  message: 'valid response must be "y" for yes or "n" for no',
-  description: 'Is the character a sexually promiscuous? (Y/N)',
-  required: true
-}
-const questionStoner = { 
-  name: 'response', 
-  pattern: /([yn])+/i,
-  message: 'valid response must be "y" for yes or "n" for no',
-  description: 'Does the character use drugs? (Y/N)',
-  required: true
-}
-const questionProtagonist = { 
-  name: 'response', 
-  pattern: /([yn])+/i,
-  message: 'valid response must be "y" for yes or "n" for no',
-  description: 'Is the character the protagonist? (Y/N)',
-  required: true
-}
-const questionNerd = { 
-  name: 'response', 
-  pattern: /([yn])+/i,
-  message: 'valid response must be "y" for yes or "n" for no',
-  description: 'Is the character a nerd? (Y/N)',
-  required: true
-}
-const questionSporty = { 
-  name: 'response', 
-  pattern: /([yn])+/i,
-  message: 'valid response must be "y" for yes or "n" for no',
-  description: 'Is the character either a member of a sports team, or in a relationship with a member of a sports team? (Y/N)',
-  required: true
-}
-const questionContinue = { 
-  name: 'response', 
-  pattern: /([yn])+/i,
-  message: 'valid response must be "y" for yes or "n" for no',
-  description: 'Would you like to add another character? (Y/N)',
-  required: true
-}
+
+const questionBlack = questionConstructor('Is the character Black?')
+const questionFirstChar = questionConstructor('Is the character the first character on screen?')
+const questionWhore = questionConstructor('Is the character a sexually promiscuous?')
+const questionToken = questionConstructor('Is the character from a minority group?')
+const questionGender = questionConstructor('Is the character the female?')
+const questionSporty = questionConstructor('Is the character either a member of a sports team, or in a relationship with a member of a sports team?')
+const questionStoner = questionConstructor('Does the character use drugs?')
+const questionNerd = questionConstructor('Is the character a nerd?')
+const questionVirgin = questionConstructor('Is the character a virgin?')
+const questionProtagonist = questionConstructor('Is the character the protagonist?')
+const questionContinue = questionConstructor('Would you like to add another character?')
+
 const questions = () => {
   prompt.start()
   prompt.get([questionName], (err, { response }) => {
@@ -225,7 +171,6 @@ const questions = () => {
   })
 }
 
-
 const nextCharacter = () => {
   prompt.get([questionContinue], (err, { response }) => {
     if (response.toLowerCase() === 'y') questions()
@@ -237,14 +182,3 @@ const nextCharacter = () => {
 }
 
 questions()
-
-
-
-
-
-
-  
-
-
-
-
